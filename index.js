2459322748a8f1505929856e8cefc0be5885cdf9
Pedro Entringer <pedrodaher1997@gmail.com>
2018-11-07 16:06:10 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+//import
+var request = require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+
+//initialize
+const app = express();
+const router = express.Router();
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const url = "http://www.jadlog.com.br/trackingdetalhe.jsp?cte=";
+const method = "GET";
+const port = 3000;
+
+//initilize routes
+router.get('/', (req, res) => {
+    res.status(200).json({status:true, api: 'Rastreio JadLog', author: "Pedro Entringer"});
+});
+
+router.get('/rastreio/:cte', (req, res) => {
+    let cte = req.params.cte;
+    request( {url: url + cte, method: method, followAllRedirects: true, jar: true} , function(error, response, body) {
+        if (error) {
+            res.status(500).json({status:false, message: 'Houve um problema ao executar a operação. Tente mais tarde.', error: error});
+        } else {
+            
+            var $ = cheerio.load(body);
+            var tracking = [];
+
+            $('table.bordas tr:not(:first-child)').each(function(i) {
+                let track = {
+                    documento: $(this).find('td').eq(0).text().trim(),
+                    data: $(this).find('td').eq(1).text().trim(),
+                    hora: $(this).find('td').eq(2).text().trim(),
+                    origem: $(this).find('td').eq(3).text().trim(),
+                    status: $(this).find('td').eq(4).text().trim(),
+                    destino: $(this).find('td').eq(5).text().trim()
+                }
+                tracking.push(track);
+            });
+
+            if(tracking.length > 0){
+                res.status(200).json({status:true, message: 'Rastreamento CT-e ' + cte, tracking: tracking});
+            }else{
+                res.status(500).json({status:false, message: 'Não existe tracking para este conhecimento'});
+            }
+        }
+    });
+});
+
+app.use('/jadlog/', router);
+app.listen(port);
+console.log('API ONLINE - ON PORT ' + port);
